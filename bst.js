33779c045fb0be9c108765868e988a61b80531fa
Robert Chen <bergintonatcapital@gmail.com>
2024-03-04 11:43:04 -0500

    Add insert and delete function to BST

--- bst.js
+++ bst.js
@@ -9,9 +9,69 @@ function Node(value) {
 function Tree(array) {
 	// Sort the array to maintain the BST property
 	array.sort((a, b) => a - b);
+
 	const root = buildTree(array);
-	prettyPrint(root); // visualize the binary search tree
-	return root;
+
+	function insert(value, root) {
+		// Always insert value as a leaf node
+		if (root === null) {
+			return Node(value);
+		} else {
+			if (value === root.value) {
+				return root;
+			} else if (value > root.value) {
+				root.right = insert(value, root.right);
+			} else {
+				root.left = insert(value, root.left);
+			}
+			return root;
+		}
+	}
+
+   function deleteItem(value, root) {
+      // Base case
+      if (root === null) {
+          return root;
+      }
+  
+      // Traverse tree until value found
+      if (value < root.value) {
+          root.left = deleteItem(value, root.left);
+          return root;
+      } else if (value > root.value) {
+          root.right = deleteItem(value, root.right);
+          return root;
+      }
+  
+      // VALUE FOUND!
+      // If node has only one child or no child
+      if (root.left === null) {
+          return root.right; // return the child's right so new parent can point to it
+      } else if (root.right === null) {
+          return root.left; // return child's left so new parent can point to it
+      }
+  
+      // VALUE FOUND!
+      // If node has two children, find the successor (next smallest value)
+      let successor = root.right;
+      while (successor.left !== null) {
+          successor = successor.left;
+      }
+  
+      // Copy the successor's value to this node
+      root.value = successor.value;
+  
+      // Delete the successor node
+      root.right = deleteItem(successor.value, root.right);
+  
+      return root;
+  }
+  
+	return {
+		root,
+		insert,
+		deleteItem,
+	};
 }
 
 function buildTree(array) {
@@ -42,4 +102,18 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 	}
 };
 
-Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const tree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(tree.root); // visualize the binary search tree
+
+console.log('INSERT')
+tree.insert(5, tree.root);
+prettyPrint(tree.root);
+
+console.log('DELETE')
+tree.deleteItem(5, tree.root);
+prettyPrint(tree.root);
+
+tree.deleteItem(67, tree.root);
+prettyPrint(tree.root);
+
+
